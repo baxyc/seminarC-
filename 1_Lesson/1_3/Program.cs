362e898a6536bfa15db5bc328f3e7a34d7bdf358
Ivan <baxyc.da@gmail.com>
2022-07-09 22:25:09 +1100

    Task 1_3

---
+++ 1_Lesson/1_3/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Write on number: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a == 1)
+{
+    Console.WriteLine("ПН");
+}
+else if (a == 2)
+{
+    Console.WriteLine("ВТ");
+}
+else if (a == 3)
+{
+    Console.WriteLine("СР");
+}
+else if (a == 4)
+{
+    Console.WriteLine("ЧТ");
+}
+else if (a == 5)
+{
+    Console.WriteLine("ПТ");
+}
+else if (a == 6)
+{
+    Console.WriteLine("СБ");
+}
+else if (a == 7)
+{
+    Console.WriteLine("ВС");
+}
+else 
+{
+    Console.WriteLine(" Ошибка ввода");
+}
