65bc64d14697b3131ea3db5ca8704650d0bebf48
Ivan <baxyc.da@gmail.com>
2022-07-14 22:19:37 +1100

    Task 2_3

---
+++ 2_Lesson/2_3/Program.cs
@@ -0,0 +1,11 @@
+﻿void Method1 (int a, int b)
+{
+    if (a%b == 0)
+        Console.WriteLine ("Кратно");
+    else
+    {
+        Console.WriteLine ($"Не кратно {a%b}");
+    }
+}
+Method1 (34,5);
+// Method1 (16,4)
