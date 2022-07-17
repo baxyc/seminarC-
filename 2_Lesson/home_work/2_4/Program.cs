6703fcd2e553853db3a73e4cf3340d38aea239a5
Ivan <baxyc.da@gmail.com>
2022-07-17 13:37:25 +1100

    HW2_4

---
+++ 2_Lesson/home_work/2_4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter the number indicating the day of the week: ");
+int c = int.Parse(Console.ReadLine());
+if (c < 1 || c > 7)
+{
+    Console.WriteLine("error");
+}
+else
+{
+    if (c < 6)
+    {
+        Console.WriteLine("Weekday");
+    }
+    else
+    {
+        Console.WriteLine("Weekend");
+    }
+}
