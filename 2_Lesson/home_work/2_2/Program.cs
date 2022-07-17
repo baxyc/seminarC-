8fe360bb2f2d51c0b2e56f87016f1ddbfb5fa88d
Ivan <baxyc.da@gmail.com>
2022-07-17 13:05:47 +1100

    HW2_2

---
+++ 2_Lesson/home_work/2_2/Program.cs
@@ -0,0 +1,10 @@
+﻿int Delete(int num)
+{
+    int A = num / 100;
+    int B = num % 10;
+    return A * 10 + B;
+}
+int a = new Random().Next(100,1000);
+Console.WriteLine(a);
+
+Console.WriteLine(Delete(a));
