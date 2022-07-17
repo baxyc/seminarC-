6fcffa58485beb0aeb49a280998e71b57e528500
Ivan <baxyc.da@gmail.com>
2022-07-17 13:01:29 +1100

    HW2_1

---
+++ 2_Lesson/home_work/1_1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Write number N: ");
+int n = int.Parse(Console.ReadLine());
+if (n > 999 ^ n < 100)
+{
+    Console.Write("error");
+}
+else 
+{
+    Console.WriteLine(n % 100 / 10);
+}
