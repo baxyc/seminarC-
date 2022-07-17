d4f25416c1213c4eb13a95260fb709fe91ba6ac5
Ivan <baxyc.da@gmail.com>
2022-07-17 13:21:36 +1100

    HW2_3

---
+++ 2_Lesson/home_work/3_3/Program.cs
@@ -0,0 +1,23 @@
+﻿void Num(int v)
+{
+    if (v < 0)
+    {
+        v = -v;
+    }
+
+    if (v < 100)
+    {
+        Console.WriteLine("There is no third digit");
+        return;
+    }
+    while (v > 1000)
+    {
+        v = v / 10;    
+    }
+    Console.WriteLine(v % 10);
+
+}
+Console.Write("Write a number N: ");
+int v = int.Parse(Console.ReadLine());
+
+Num(v);
