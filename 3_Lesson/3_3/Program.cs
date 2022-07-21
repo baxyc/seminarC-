e6fa3f6f5ee9430e5d0a03aef6b22c79ec377ca6
Ivan <baxyc.da@gmail.com>
2022-07-21 23:18:12 +1100

    3_3 Lesson

---
+++ 3_Lesson/3_3/Program.cs
@@ -0,0 +1,6 @@
+﻿double Distance (double x1, double y1, double x2, double y2 )
+{
+    return Math.Round(Math.Sqrt(Math.Pow(x1 - x2,2) + Math.Pow(y1 - y2,2)),2);
+}
+
+Console.WriteLine (Distance(3, 6, 2, 1));
