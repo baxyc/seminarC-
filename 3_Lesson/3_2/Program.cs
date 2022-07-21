a08ee3033293e77f65bb44420151f9e82e464085
Ivan <baxyc.da@gmail.com>
2022-07-21 23:03:48 +1100

    3_1! Lesson

---
+++ 3_Lesson/3_2/Program.cs
@@ -0,0 +1,20 @@
+﻿void Quarters(int a)
+{
+    if (a == 1 )
+        Console.WriteLine("x > 0 && y > 0");
+    else if (a == 2)
+        Console.WriteLine("x < 0 && y > 0");
+    else if (a == 3)
+        Console.WriteLine("x < 0 && y < 0");
+    else if (a == 4)
+        Console.WriteLine("x > 0 && y < 0");
+    else 
+        Console.WriteLine("error");
+    
+}
+
+Quarters(1);
+Quarters(2);
+Quarters(3);
+Quarters(4);
+Quarters(-9);
