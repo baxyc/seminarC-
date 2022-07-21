7ae591b7556494e1e8e7526e5cb34095ece5501e
Ivan <baxyc.da@gmail.com>
2022-07-21 23:37:09 +1100

    3_3 HW

---
+++ 3_Lesson/home_work/3_3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter N: ");
+int N=int.Parse(Console.ReadLine());
+int CubeTable(int a)
+{
+    int i=1;
+    for(i=1;i<=a;i++)
+        Console.Write($"{Math.Pow(i,3)} ");
+    return(0);
+}
+CubeTable(N);
