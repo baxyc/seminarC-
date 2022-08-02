c87d290f0680787186be0c81d43d83594b29c984
Ivan <baxyc.da@gmail.com>
2022-08-03 10:23:15 +1100

    HW_4_2

---
+++ 4_Lesson/home_work/4_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int DigitSum(int num)
+{
+    int Sum = 0;
+    for(int i = 1; num > 0; i++)
+    {
+    Sum += num % 10;
+    num /= 10;
+    }
+return Sum;
+}
+Console.WriteLine(DigitSum(452));
