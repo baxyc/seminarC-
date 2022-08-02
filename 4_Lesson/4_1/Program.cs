28fb702316e8cbe0006fcdcbf026c569181f9c75
Ivan <baxyc.da@gmail.com>
2022-08-03 09:18:27 +1100

    Task_4_1

---
+++ 4_Lesson/4_1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход число и выдает количество цифр в числе
+
+void Length (int a)
+{
+    string num = a.ToString();
+    Console.WriteLine (num.Length);
+}
+Length(1670);
