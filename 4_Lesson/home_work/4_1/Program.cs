54cc950440af4439ff446ce5e9471659f36b2680
Ivan <baxyc.da@gmail.com>
2022-08-03 10:10:50 +1100

    HW_4_1

---
+++ 4_Lesson/home_work/4_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+int NumExp(int num, int a)
+{
+    int Mult = 1;
+    for(int i = 1; i <= a; i++)
+        Mult *= num;
+    return Mult;
+}
+Console.WriteLine(NumExp(3,5));
