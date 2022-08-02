1da0216866580b39526ea41a8d0e8c787aa5f852
Ivan <baxyc.da@gmail.com>
2022-08-03 09:58:46 +1100

    Task_4_3

---
+++ 4_Lesson/4_3/Program.cs
@@ -0,0 +1,11 @@
+﻿void Random01Array(int num)
+{
+    int[] array = new int[num];
+    for(int i=0; i < num; i++)
+    {
+        array[i]=new Random().Next(0, 2);
+        Console.Write($"{array[i]} ");
+    }
+
+}
+Random01Array(8);
