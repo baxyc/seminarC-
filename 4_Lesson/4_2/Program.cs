32c0ce9eb0b4ec8d8106e8e31f0917b28e1adc9a
Ivan <baxyc.da@gmail.com>
2022-08-03 09:30:43 +1100

    Task_4_2

---
+++ 4_Lesson/4_2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N.
+
+int Method(int num)
+{
+    int AllSum = 1;
+    for (int i = 1; i <= num; i++)
+        AllSum = AllSum * i;
+    return AllSum;
+}
+Console.WriteLine(Method(4));
