c7e8baa055879c7405d05898f7c3ce431ba5367e
Ivan <baxyc.da@gmail.com>
2022-08-14 10:31:31 +1100

    HW6_3

---
+++ 6_Lesson/home_work/6_3/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+int Check(int num)
+{
+    int count = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        Console.Write($"Число {i}; ");
+        int a = int.Parse(Console.ReadLine());
+        if (a > 0) count++;
+    }
+    return count;
+}
+Console.Write("Количество чисел?: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine($"Количество чисел больше 0: {Check(a)}");
