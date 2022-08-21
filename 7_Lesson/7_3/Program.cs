40adfedfab98de0b8dd824a0ea52f3c804b906cd
Ivan <baxyc.da@gmail.com>
2022-08-22 09:13:52 +1100

    Task7_3

---
+++ 7_Lesson/7_3/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 3: Задайте двумерный массив. Найдите сумму элементов главной диагонали.
+
+int[,] fillArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-10, 11);
+        }
+    }
+    return array;
+}
+
+int findSumOfDiagonalElements(int[,] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                result += array[i, j];
+            }
+        }
+    }
+    return result;
+}
+
+void printArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = fillArray(4, 4);
+printArray(array);
+Console.WriteLine();
+Console.Write($"Sum of diagonal elements: {findSumOfDiagonalElements(array)}");
