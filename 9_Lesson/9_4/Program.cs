fb1cb2b7ee561753ec7b409e77b333205e8de776
Ivan <baxyc.da@gmail.com>
2022-08-22 10:31:32 +1100

    Task9_3

---
+++ 9_Lesson/9_4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+
+int Pow(int num, int mult)
+{
+    if(mult==0) return 1;
+    return Pow(num, mult-1) *num;
+
+}
+Console.WriteLine(Pow(5,4));
