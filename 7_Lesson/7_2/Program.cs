6311ef1f0a289456b66d6c45b581efab0420856e
Ivan <baxyc.da@gmail.com>
2022-08-22 09:09:11 +1100

    Task7_2

---
+++ 7_Lesson/7_2/Program.cs
@@ -0,0 +1,45 @@
+﻿// 2. Задайте двумерный массив. Найдите элементы,
+// у которых обе позиции чётные, и замените эти элементы на их квадраты.
+
+
+int[,] fillArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-10, 11);
+        }
+    }
+    return array;
+}
+
+int[,] doubleEvenElements(int[,] array)
+{
+    for (int i = 1; i < array.GetLength(0); i += 2)
+    {
+        for (int j = 1; j < array.GetLength(1); j += 2)
+        {
+            array[i, j] *= array[i, j];
+        }
+    }
+    return array;
+}
+
+void printArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = fillArray(3, 4);
+printArray(array);
+Console.WriteLine();
+printArray(doubleEvenElements(array));
