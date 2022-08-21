a217efd7609c171de69455471e1f778e9fd94eb0
Ivan <baxyc.da@gmail.com>
2022-08-22 10:30:34 +1100

    Task9_3

---
+++ 9_Lesson/9_3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр. Использовать рекурсию.
+
+int DigitSum(int num)
+{
+    if(num==0) return 0;
+    return DigitSum(num/10) + num%10;
+
+}
+Console.WriteLine(DigitSum(123));
