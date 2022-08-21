0fb4bbad4c96d0c4bc61d566fb10c559dd584a09
Ivan <baxyc.da@gmail.com>
2022-08-22 09:24:36 +1100

    HW7_4

---
+++ 7_Lesson/HW/7_1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 1: Задайте двумерный массив размером m×n,заполненный случайными вещественными числами.
+
+double[,] fillArray()
+{
+    Console.Write("Enter number of rows: ");
+    int m = int.Parse(Console.ReadLine());
+    Console.Write("Enter number of columns: ");
+    int n = int.Parse(Console.ReadLine());
+    double[,] array = new double[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round(new Random().NextDouble() * (100 + 150) - 100, 2); ;
+        }
+    }
+    return array;
+}
+
+void printArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+printArray(fillArray());
