d5e59b0a8abf6bf5420d43f06530e0d1a16fcd3d
Ivan <baxyc.da@gmail.com>
2022-08-22 10:09:52 +1100

    Task8_2

---
+++ 8_Lesson/8_2/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 2: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это
+// невозможно, программа должна вывести сообщение для пользователя.
+
+int[,] fillArray()
+{
+    Console.Write("Enter number of rows: ");
+    int m = int.Parse(Console.ReadLine());
+    Console.Write("Enter number of columns: ");
+    int n = int.Parse(Console.ReadLine());
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-10, 11);
+        }
+    }
+    return array;
+}
+
+void printArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] changeRowsToColumns(int[,] array)
+{
+    int temp = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (array.GetLength(0) != array.GetLength(1))
+            {
+                Console.WriteLine("Number of rows & columns must be equally. Printing incoming array: ");
+                break;
+            }
+            else
+            {
+                temp = array[i, j];
+                array[i, j] = array[j, i];
+                array[j, i] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+int[,] array = fillArray();
+printArray(array);
+Console.WriteLine();
+printArray(changeRowsToColumns(array));
